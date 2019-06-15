4a449d98f6fc5c32194201a936c9f3b210b1999f
vanessa <32312712+vlbee@users.noreply.github.com>
2019-06-15 20:13:44 +0100

    Refactor BoxApp - Add state - Extract Header from BoxApp - Add Header Component

--- src/BoxApp/BoxApp.js
+++ src/BoxApp/BoxApp.js
@@ -1,84 +1,88 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import Logo from "../Logo.png";
+import { Header } from "./Header";
 
-const Box = styled("div")`
+const App = styled("div")`
   text-align: center;
 `;
 
-const CollectionPoint = styled("header")`
-  padding: 10px;
-`;
-
-const UpcomingCollection = styled("section")`
+const Col = styled("div")`
   display: flex;
-  justify-content: space-evenly;
-  margin: 10px;
-  background-color: lightgray;
-  padding: 10px;
+  flex-direction: column;
+  align-items: center;
 `;
 
-const NextCollection = styled(UpcomingCollection)`
-  vertical-align: middle;
-`;
-
-const Title = styled("h2")`
-  flex: 1;
-  text-align: left;
-  margin-left: 20px;
-  padding: 1em 0;
-  width: 100%;
-  font-size: 1em;
-`;
-
-const Time = styled("h2")`
-  margin-right: 20px;
-`;
-
-const UrgentReqest = styled("button")`
+const Request = styled("button")`
   font-size: 32px;
-  background-color: lightcoral;
+  background-color: orange;
   color: white;
-  padding: 30px;
+  font-weight: bold;
+  padding: 20px 15px;
+  margin: 15px;
   margin-top: 30px;
+  min-width: 85vw;
+  @media (min-width: 768px) {
+    min-width: 300px;
+    max-width: 400px;
+  }
 `;
+
+const RequestUrgent = styled(Request)`
+  background-color: orangered;
+  margin-top: 10px;
+`;
+
+const NextCollection = styled("section")`
+  background-color: lightgray;
+  margin: 15px 15px 0 15px;
+  padding-top: 10px;
+  min-width: 85vw;
+  @media (min-width: 768px) {
+    min-width: 300px;
+    max-width: 400px;
+  }
+`;
+
+const Title = styled.h2`
+  font-size: 1em;
+  color: ${props => (props.urgent ? "orangered" : null)};
+`;
+const Time = styled.h2`
+  color: ${props => (props.urgent ? "orangered" : null)};
+`;
+
+const renderNext = ({ urgent, next, nextDelivery }) => (
+  <NextCollection>
+    <Title urgent={urgent}>
+      {urgent ? "Next Collection (URGENT)" : "Next Collection"}
+    </Title>
+    <Time urgent={urgent}>{next}</Time>
+    <Title>Delivery Window</Title>
+    <Time>{nextDelivery}</Time>
+  </NextCollection>
+);
 
 function BoxApp({ match }) {
   const state = {
     next: "17:00",
     nextDelivery: "17:00",
+    urgent: true,
     upcoming: "17:00",
     upcomingDelivery: "17:00"
   };
   const [pickUps, updatePickUps] = useState(state); // delivery or ion
 
   return (
-    <NextCollection>
-      <CollectionPoint>
-        <img src={Logo} alt="porteroo logo" width={300} />
-        <h1>Collection Point</h1>
-        <h2> WARD {match.params.ward} </h2>
-      </CollectionPoint>
+    <App>
+      <Header ward={match.params.ward} />
       <main>
-        <div>
-          <NextCollection>
-            <Title>Next Collection</Title>
-            <Time>{pickUps.next}</Time>
-            <Title>Delivery Window</Title>
-            <Time>{pickUps.nextDelivery}</Time>
-          </NextCollection>
-          <UpcomingCollection>
-            <Title>Upcoming Collection</Title>
-            <Time>{pickUps.upcoming}</Time>
-            <Title>Delivery Window</Title>
-            <Time>{pickUps.upcomingDelivery}</Time>
-          </UpcomingCollection>
-        </div>
-        <div>
-          <UrgentReqest>Urgent Collection Request</UrgentReqest>
-        </div>
+        <Col>
+          {pickUps.next ? renderNext(pickUps) : "No upcoming collections"}
+          <Request>Request</Request>
+          <RequestUrgent>Urgent Request</RequestUrgent>
+        </Col>
       </main>
-    </NextCollection>
+    </App>
   );
 }
 
---
+++ src/BoxApp/Header.js
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "styled-components";
+import pogo from "../Logo.png";
+
+const Layout = styled("div")`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 20px;
+
+  & h1,
+  h2 {
+    margin: 0;
+  }
+`;
+
+const Logo = styled("img")`
+  margin-bottom: 20px;
+  max-width: 300px;
+  @media (max-width: 425px) {
+    max-width: 75vw;
+  }
+`;
+
+export const Header = ({ ward }) => {
+  return (
+    <Layout>
+      <Logo src={pogo} alt="porteroo logo" />
+      <div>
+        <h2>Collection Point</h2>
+        <h1> WARD {ward} </h1>
+      </div>
+    </Layout>
+  );
+};
